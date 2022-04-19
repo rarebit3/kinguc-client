11dc32c71596aa92f38ae6ae2525ea024d954f72
David Wu <dwu0508@gmail.com>
2022-04-19 17:16:20 -0500

    add castle working

--- kinguc/src/pages/Castles.js
+++ kinguc/src/pages/Castles.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { GetCastles } from '../services/CastleService'
 import { useNavigate } from 'react-router-dom'
 
-const Castles = (user, authenticated) => {
+const Castles = ({user, authenticated}) => {
   let navigate = useNavigate()
   const [castles, setCastles] = useState([])
   
@@ -16,20 +16,20 @@ const Castles = (user, authenticated) => {
   
   return ( user && authenticated ) ? (
     <div className="grid">
-        <button onClick={() => navigate('/addcastle')}>Add a Castle</button>
+        <button onClick={() => navigate(`/addcastle/${user.id}`)}>Add a Castle</button>
       {castles.map((castle) => (
         <div className="card" key={castle.id}>
           <h3>{castle.name}</h3>
-          <h3>{castle.forType}</h3>
+          <h3>{castle.forTypes}</h3>
           <h3>{castle.servantCount}</h3>
           <h3>{castle.incomePerCastle}</h3>
-          <h3>{castle.region}</h3>
+          <h3>{castle.location.name}</h3>
           <div>
             <img src={castle.image} alt="castle"/>
           </div>
         </div>
       ))}
-            <button onClick={() => navigate('/addcastle')}>Add a Castle</button>
+            {/* <button onClick={() => navigate('/addcastle')}>Add a Castle</button> */}
     </div>
   ) : (
     <div className="grid">
--- kinguc/src/services/UserService.js
+++ kinguc/src/services/UserService.js
@@ -1,8 +1,8 @@
 import Client from './api'
 
-export const GetUser = async () => {
+export const GetUser = async (userId) => {
     try {
-      const res = await Client.get('api/user')
+      const res = await Client.get(`api/user/${userId}`)
       return res.data
     } catch (error) {
       throw error
