ae787c6df325e630a952111c4b5e4d4150fd204b
rarebit3 <johntrevorbond@gmail.com>
2022-04-21 13:58:49 -0400

    deployment main

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
-// export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'http://localhost:3001'
 
-let BASE_URL = process.env.NODE_ENV === 'production' ? 'https://kinguc-server.herokuapp.com/' : 'https://localhost:3001'
+// let BASE_URL = process.env.NODE_ENV === 'production' ? 'https://kinguc-server.herokuapp.com/' : 'https://localhost:3001'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
