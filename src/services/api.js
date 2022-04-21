401acb79082e3d87e890af5f336d0184c916f73a
rarebit3 <johntrevorbond@gmail.com>
2022-04-21 16:57:17 -0400

    local env test

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import Axios from 'axios'
 
-// export const BASE_URL = 'http://localhost:3001'
 
-export const BASE_URL = 'https://kinguc-server.herokuapp.com'
 
-// let BASE_URL = process.env.NODE_ENV === 'production' ? 'https://kinguc-server.herokuapp.com' : 'https://localhost:3001'
+let BASE_URL = process.env.NODE_ENV === 'local' ? 'https://localhost:3001' : 'https://kinguc-server.herokuapp.com'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
